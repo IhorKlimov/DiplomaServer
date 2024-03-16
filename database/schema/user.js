05358e39f8c9240d74e30175481508d515981613
Ihor Klimov <iklimov6@gmail.com>
2024-03-16 09:08:16 -0400

    Make email and userName unique in User model

--- database/schema/user.js
+++ database/schema/user.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    userName: { type: String, required: true },
-    email: { type: String, required: true },
+    userName: { type: String, required: true, unique: true, },
+    email: { type: String, required: true, unique: true, },
     password: { type: String, required: true },
     imageUrl: { type: String }
 });
