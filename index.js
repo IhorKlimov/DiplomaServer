0b0d80f5a2af70ddb18a0f0e598f9dc8b4508bdf
Ihor Klimov <iklimov6@gmail.com>
2024-03-16 17:28:08 -0400

    Add edit recipe endpoint

--- index.js
+++ index.js
@@ -121,7 +121,6 @@ app.get('/recipe', async (req, res) => {
 
         res.send(recipe);
     } catch (e) {
-        console.log(e)
         res.status(500).send(e.message);
     } finally {
         // await client.close();
@@ -176,6 +175,40 @@ app.post('/recipe', async (req, res) => {
         });
         const model = await recipe.save();
         res.send({ recipeId: model._id, });
+    } catch (e) {
+        res.status(400).send(e.message);
+    } finally {
+        // await client.close();
+    }
+});
+
+
+app.put('/recipe', async (req, res) => {
+    try {
+        const userId = session.getUserId(req.get('session'));
+        const recipeId = req.body.recipeId;
+        if (!userId) {
+            res.status(401).send('Unauthorized. Missing user id');
+            return;
+        }
+        if (!recipeId) {
+            res.status(400).send('Missing attributes');
+            return;
+        }
+
+        const recipe = await Recipe.findById(recipeId);
+        if (recipe.authorId != userId) {
+            res.status(401).send('Unauthorized. This recipe does not belog to this user');
+            return;
+        }
+
+        await Recipe.findByIdAndUpdate(recipeId, {
+            title: req.body.title,
+            imageUrl: req.body.imageUrl,
+            description: req.body.description,
+        });
+
+        res.send({ status: 'Saved changes' });
     } catch (e) {
         res.status(400).send(e.message);
     } finally {
