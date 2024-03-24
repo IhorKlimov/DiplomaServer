8a543c91de9b2db86206f93a2ee1f5b4c07a27be
Ihor Klimov <iklimov6@gmail.com>
2024-03-23 21:39:03 -0400

    Extend recipe endpoints with categories

--- model/recipe.js
+++ model/recipe.js
@@ -4,6 +4,7 @@ const recipeSchema = new mongoose.Schema({
     title: { type: String, required: true },
     imageUrl: { type: String, required: true },
     description: { type: String, required: true },
+    categories: { type: Array, required: true, },
     authorId: { type: String, required: true }
 });
 const Recipe = mongoose.model('recipes', recipeSchema);
