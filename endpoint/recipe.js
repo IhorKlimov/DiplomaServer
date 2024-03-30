5e73493ed30cb650a596b5e4763b2d8f4469d673
Ihor Klimov <iklimov6@gmail.com>
2024-03-30 10:20:55 -0400

    Refactoring

--- endpoint/recipe.js
+++ endpoint/recipe.js
@@ -16,124 +16,8 @@ module.exports = function (app) {
         const sortBy = req.query.sortBy;
         const page = req.query.page;
 
-        const pipelines = [
-            { "$match": { "$expr": { "$eq": ["$_id", "$$authorObjectId"] } } },
-        ];
-
-        if (showMyRecipes === 'true') {
-            userId = session.getUserId(req.get('session'));
-            if (!userId) {
-                res.status(401).send('Unauthorized. Missing user id');
-                return;
-            }
-        }
-
-        if (userId) {
-            pipelines.push({
-                "$match": { "$expr": { "$eq": ["$_id", new ObjectId(userId)] } }
-            });
-        }
-
-        const aggregates = [];
-        if (query) {
-            aggregates.push({
-                "$match": {
-                    "$or": [
-                        { "title": { "$regex": `.*${query}.*`, "$options": 'i' }, },
-                        { "description": { "$regex": `.*${query}.*`, "$options": 'i' }, }
-                    ]
-                }
-            });
-        }
-        if (categories) {
-            const c = categories.split(',').map(e => new ObjectId(e));
-            aggregates.push({ "$match": { "categories": { "$in": c } }, });
-        }
-        if (cookingMethods) {
-            const c = cookingMethods.split(',').map(e => new ObjectId(e));
-            aggregates.push({ "$match": { "cookingMethods": { "$in": c } }, });
-        }
-        if (difficulty) {
-            aggregates.push({ "$match": { "difficulty": { "$eq": new ObjectId(difficulty) } }, });
-        }
-        if (sortBy) {
-            const sortOption = await SortOption.findById(sortBy);
-            if (sortOption.field.indexOf('&') != -1) {
-                const s = {};
-                sortOption.field.split('&').forEach((e) => {
-                    s[e] = sortOption.order;
-                });
-                aggregates.push({ "$sort": s },);
-            } else {
-                aggregates.push({ "$sort": { [sortOption.field]: sortOption.order, } },);
-            }
-        }
-
-        aggregates.push(
-            {
-                "$lookup": {
-                    "let": { "authorObjectId": { "$toObjectId": "$authorId" } },
-                    "from": "users",
-                    "pipeline": pipelines,
-                    "as": "author"
-                }
-            },
-            {
-                "$lookup": {
-                    "from": "categories",
-                    "localField": "categories",
-                    "foreignField": "_id",
-                    "as": "categories",
-                },
-            },
-            {
-                "$lookup": {
-                    "from": "difficulties",
-                    "localField": "difficulty",
-                    "foreignField": "_id",
-                    "as": "difficulty",
-                },
-            },
-            {
-                "$lookup": {
-                    "from": "cookingmethods",
-                    "localField": "cookingMethods",
-                    "foreignField": "_id",
-                    "as": "cookingMethods",
-                },
-            },
-            { "$unwind": { path: "$difficulty" } },
-            { "$unwind": { path: "$author" } },
-            {
-                $facet: {
-                    results: [
-                        { $skip: (page - 1) * pageSize },
-                        { $limit: pageSize },
-                    ],
-                    count: [
-                        { $group: { _id: null, count: { $sum: 1 } } }
-                    ]
-                }
-            },
-            {
-                $project: {
-                    count: { $arrayElemAt: ['$count.count', 0] },
-                    recipes: '$results'
-                }
-            },
-            {
-                $addFields: {
-                    hasMoreData: {
-                        $cond: {
-                            if: { $gt: ['$count', (page * pageSize)] },
-                            then: true,
-                            else: false
-                        }
-                    }
-                }
-            },
-        );
-
+        const aggregates = await buildAggregate(userId, query, categories, cookingMethods, difficulty, showMyRecipes, sortBy, page);
+        
         try {
             const data = await Recipe.aggregate(aggregates).exec();
             res.send(data[0]);
@@ -314,4 +198,126 @@ module.exports = function (app) {
         }
     })
 
+}
+
+async function buildAggregate(userId, query, categories, cookingMethods, difficulty, showMyRecipes, sortBy, page) {
+    const pipelines = [
+        { "$match": { "$expr": { "$eq": ["$_id", "$$authorObjectId"] } } },
+    ];
+
+    if (showMyRecipes === 'true') {
+        userId = session.getUserId(req.get('session'));
+        if (!userId) {
+            res.status(401).send('Unauthorized. Missing user id');
+            return;
+        }
+    }
+
+    if (userId) {
+        pipelines.push({
+            "$match": { "$expr": { "$eq": ["$_id", new ObjectId(userId)] } }
+        });
+    }
+
+    const aggregates = [];
+    if (query) {
+        aggregates.push({
+            "$match": {
+                "$or": [
+                    { "title": { "$regex": `.*${query}.*`, "$options": 'i' }, },
+                    { "description": { "$regex": `.*${query}.*`, "$options": 'i' }, }
+                ]
+            }
+        });
+    }
+    if (categories) {
+        const c = categories.split(',').map(e => new ObjectId(e));
+        aggregates.push({ "$match": { "categories": { "$in": c } }, });
+    }
+    if (cookingMethods) {
+        const c = cookingMethods.split(',').map(e => new ObjectId(e));
+        aggregates.push({ "$match": { "cookingMethods": { "$in": c } }, });
+    }
+    if (difficulty) {
+        aggregates.push({ "$match": { "difficulty": { "$eq": new ObjectId(difficulty) } }, });
+    }
+    if (sortBy) {
+        const sortOption = await SortOption.findById(sortBy);
+        if (sortOption.field.indexOf('&') != -1) {
+            const s = {};
+            sortOption.field.split('&').forEach((e) => {
+                s[e] = sortOption.order;
+            });
+            aggregates.push({ "$sort": s },);
+        } else {
+            aggregates.push({ "$sort": { [sortOption.field]: sortOption.order, } },);
+        }
+    }
+
+    aggregates.push(
+        {
+            "$lookup": {
+                "let": { "authorObjectId": { "$toObjectId": "$authorId" } },
+                "from": "users",
+                "pipeline": pipelines,
+                "as": "author"
+            }
+        },
+        {
+            "$lookup": {
+                "from": "categories",
+                "localField": "categories",
+                "foreignField": "_id",
+                "as": "categories",
+            },
+        },
+        {
+            "$lookup": {
+                "from": "difficulties",
+                "localField": "difficulty",
+                "foreignField": "_id",
+                "as": "difficulty",
+            },
+        },
+        {
+            "$lookup": {
+                "from": "cookingmethods",
+                "localField": "cookingMethods",
+                "foreignField": "_id",
+                "as": "cookingMethods",
+            },
+        },
+        { "$unwind": { path: "$difficulty" } },
+        { "$unwind": { path: "$author" } },
+        {
+            $facet: {
+                results: [
+                    { $skip: (page - 1) * pageSize },
+                    { $limit: pageSize },
+                ],
+                count: [
+                    { $group: { _id: null, count: { $sum: 1 } } }
+                ]
+            }
+        },
+        {
+            $project: {
+                count: { $arrayElemAt: ['$count.count', 0] },
+                recipes: '$results'
+            }
+        },
+        {
+            $addFields: {
+                hasMoreData: {
+                    $cond: {
+                        if: { $gt: ['$count', (page * pageSize)] },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+    );
+
+    return aggregates;
 }
